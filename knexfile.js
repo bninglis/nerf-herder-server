d6c8fb7aa09ba5822eb188a94c964e2dbcb8be9a
bninglis <107963185+bninglis@users.noreply.github.com>
2022-11-25 02:39:56 -0800

    readies for deployment

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,14 @@
 // Update with your config settings.
+require("dotenv").config();
+console.log(process.env.DB_HOST);
 
 module.exports = {
     client: "mysql",
     connection: {
-        host: "127.0.0.1",
-        user: "root",
-        password: "rootroot",
-        database: "nerf_herder",
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         charset: "utf8",
     },
 };
